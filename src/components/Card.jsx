dd294dc78f7aabd8f7cdaf88a4ce7885c8d45867
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-10-31 16:27:25 +0700

    covid19-tracker

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const Cards = (props) => {
+
+  const confirmed = props.data.confirmed?props.data.confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'):0
+  const recovered = props.data.recovered?props.data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'):0
+  const deaths = props.data.deaths?props.data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'):0
   return(
     <>
     {/* Responsive Dekstop */}
@@ -10,7 +14,7 @@ const Cards = (props) => {
           <div className="card text-left" style={{ width: '15rem'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Kasus</h6>
-              <h5 className="">{props.data.confirmed}</h5>
+              <h5 className="">{confirmed}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka kasus COVID - 19</h5>
             </div>
@@ -19,7 +23,7 @@ const Cards = (props) => {
           <div className="card text-left ml-5" style={{ width: '15rem'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Sembuh</h6>
-              <h5 className="">{props.data.recovered}</h5>
+              <h5 className="">{recovered}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka sembuh COVID - 19</h5>
             </div>
@@ -28,7 +32,7 @@ const Cards = (props) => {
           <div className="card text-left ml-5" style={{ width: '15rem'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Meninggal</h6>
-              <h5 className="">{props.data.deaths}</h5>
+              <h5 className="">{deaths}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka meninggal COVID - 19</h5>
             </div>
@@ -44,7 +48,7 @@ const Cards = (props) => {
           <div className="card text-left" style={{ width: '90%'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Kasus</h6>
-              <h5 className="">{props.data.confirmed}</h5>
+              <h5 className="">{confirmed}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka kasus COVID - 19</h5>
             </div>
@@ -55,7 +59,7 @@ const Cards = (props) => {
           <div className="card text-left" style={{ width: '90%'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Sembuh</h6>
-              <h5 className="">{props.data.recovered}</h5>
+              <h5 className="">{recovered}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka sembuh COVID - 19</h5>
             </div>
@@ -66,7 +70,7 @@ const Cards = (props) => {
           <div className="card text-left" style={{ width: '90%'}}>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">Meninggal</h6>
-              <h5 className="">{props.data.deaths}</h5>
+              <h5 className="">{deaths}</h5>
               <h6 className="card-subtitle mb-2 text-muted">Orang</h6>
               <h5 className="card-text">Jumlah angka meninggal COVID - 19</h5>
             </div>
