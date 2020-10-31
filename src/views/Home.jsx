f29e7b576a38d776d6a20d0c0c186a7dc8099e01
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-10-31 15:37:40 +0700

    covid19 tracker

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,22 +4,13 @@ import { useSelector, useDispatch } from "react-redux";
 import imageHeader from '../assets/img/head3.png'
 import { Image } from 'react-bootstrap'
 
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+
 
 
 import Cards from '../components/Card'
 
 import { GET_ALL_COUNTRIES, GET_ALL_DATA_COVID} from '../redux/actions/countries'
 
-const useStyles = makeStyles((theme) => ({
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
 
 
 
@@ -27,8 +18,9 @@ const Home = () => {
   const dispatch = useDispatch()
   const history = useHistory()
 
+ 
+
   const data = useSelector((state) => state.countries)
-  const classes = useStyles();
   const [countries, setCountries] = useState()
 
   const dataCovid = {
@@ -40,7 +32,8 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(GET_ALL_COUNTRIES())
-  }, [dispatch])
+    history.push(`?country=${'Global'}`)
+  }, [dispatch, history, history.push])
 
   useEffect(() => {
     dispatch(GET_ALL_DATA_COVID(countries))
@@ -51,43 +44,59 @@ const Home = () => {
       history.push(`?country=${e.target.value}`)
     }
 
+    const dateNow = new Date(dataCovid.lastUpdate ? dataCovid.lastUpdate.slice(0,10) : Date.now())
+    const yearNow = dateNow.getFullYear()
+  
+    let month = new Array([]);
+    month[0] = "Januari";
+    month[1] = "Februari";
+    month[2] = "Maret";
+    month[3] = "April";
+    month[4] = "Mei";
+    month[5] = "Juni";
+    month[6] = "Juli";
+    month[7] = "Agustus";
+    month[8] = "September";
+    month[9] = "Oktober";
+    month[10] = "November";
+    month[11] = "Desember";
+    const monthNow = month[dateNow.getMonth()];
+    const dayNow = dateNow.getDay()
+  
+    const lastUpdate = `${dayNow} ${monthNow} ${yearNow}`
+
   return (
     <div className="container-fluid">
-      <div className="row text-center">
+      <div className="row text-center" style={{marginBottom:'70px'}}>
         <div className="col-lg-12">
-          <Image src={imageHeader} alt="imageHeader" width='400px' fluid />
+          <Image src={imageHeader} alt="imageHeader" width='350px' fluid />
           <div className="row d-flex justify-content-center">
-            <div className="col-lg-12 col-11">
-              <p>Terakhir Update : {dataCovid.lastUpdate ? dataCovid.lastUpdate.slice(0,10) : ''}</p>
+            <div className="col-lg-4 col-11">
+              <p>Terakhir Update : { lastUpdate }</p>
               <div className="mb-4 mt-3">
-                <FormControl className="col-lg-3 col-sm-4 col-11">
-                  <InputLabel shrink id="countries-label">
-                    Negara
-                  </InputLabel>
-                  <Select
-                    labelId="countries-label"
-                    id="demo-simple-select-placeholder-label"
-                    value={countries}
-                    onChange={handleChange}
-                    displayEmpty
-                    className={classes.selectEmpty}
-                  >
-                    <MenuItem value="Global">
-                      Global
-                    </MenuItem>
+                <select onChange={handleChange} className="form-control">
+                  <option value="Global">Global</option>
                     {
                     data.listCountries.map((item, index) => (
-                    <MenuItem key={index} value={item.name}>{item.name}</MenuItem>
+                    <option key={index} value={item.name ? item.name : 'Global'}>{item.name}</option>
                     ))
-                  }
-                  </Select>
-                </FormControl>
+                    }
+                </select>
               </div>
+                {                  
+                  data.isLoading ? 
+                  <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div> 
+                  :
+                  ''
+                }
             </div>
           </div>
           <Cards data={dataCovid}/>
         </div>
       </div>
+      <div className="fixed-bottom text-center text-white-50" style={{padding: '13px',backgroundColor: '#7F90FC'}}>Copyright 2020 | Riza Radia Rivaldo</div>
     </div>
   )
 }
